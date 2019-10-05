0881f548f1cbbced6617417893ca016195cb4033
Param Raval <52950939+paramraval@users.noreply.github.com>
2019-10-05 16:26:35 +0530

    Add files via upload

---
+++ Watermelon/Watermelon.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ 
+public class Watermelon{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int w = s.nextInt();
+        if(w!=2){
+            if(w%2 == 0){
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+        }
+        if(w == 2){
+            System.out.println("NO");
+        }
+        s.close();
+    }
+}
